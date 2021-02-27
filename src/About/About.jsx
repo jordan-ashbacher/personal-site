e14e679906492a8cc3a0519d84103763c911ac46
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-02-27 14:57:43 -0600

    create about page with social links

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,9 +1,72 @@
-import './About.css'
+import "./About.css"
+import { Link } from "react-router-dom"
+import { makeStyles } from '@material-ui/core/styles'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+
+const useStyles = makeStyles({
+    aboutContainer: {
+        height: "100vmin",
+        display: "flex",
+        flexFlow: "column",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    bioContainer: {
+        display: "flex",
+        flexFlow: "column"
+    },
+    bio: {
+        color: "#fafafa",
+        fontSize: "3rem",
+    },
+    linkItem: {
+        textDecoration: "none",
+        color: "#fafafa",
+        fontSize : "2.5rem",
+        marginBottom: "15px",
+    },
+    socialIcon: {
+        height: "3rem",
+        width: "3rem",
+        color: "#fafafa",
+        position: "relative",
+        top: "8px",
+        marginRight: "10px",
+    },
+    socialText: {
+        verticalAlign: "text-top"
+    }
+})
 
 const About = () => {
+    const classes = useStyles()
 
-    return(
-        <h1>About</h1>
+    return (
+        <div className={classes.aboutContainer}>
+            <div className={classes.bioContainer}>
+                <p className={classes.bio}>
+                    A full stack developer passionate about creating positive
+                    and inclusive experiences through technology and design.
+                </p>
+                <a 
+                    href="https://github.com/jordan-ashbacher"
+                    target="_blank"
+                    className={classes.linkItem}
+                > 
+                    <GitHubIcon className={classes.socialIcon}/>
+                    <span className={classes.socialText}>GitHub</span>
+                </a>
+                <a 
+                    href="https://www.linkedin.com/in/jordan-ashbacher/"
+                    target="_blank"
+                    className={classes.linkItem}
+                > 
+                    <LinkedInIcon className={classes.socialIcon}/>
+                    <span className={classes.socialText}>LinkedIn</span>
+                </a>
+            </div>
+        </div>
     )
 }
 
